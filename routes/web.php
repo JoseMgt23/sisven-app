95b0143bd8b938e70815195cca0cdda96e8da99f
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-04-25 16:00:59 -0500

    Ruta de customers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,12 @@ Route::get('/categories/create', [CategoryController ::class,'create'])->name('c
 Route::get('/categories/{category}', [CategoryController ::class,'destroy'])->name('categories.destroy');
 Route::get('/categories/{category}', [CategoryController ::class,'update'])->name('categories.update');
 Route::get('/categories/{category}/edit', [CategoryController ::class,'edit'])->name('categories.edit');
+
+//Custmoers
+
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+Route::get('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
