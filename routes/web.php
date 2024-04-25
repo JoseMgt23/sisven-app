34f07b141492fe2a66b3d38ff59f60d9fe4c68c1
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-04-25 12:20:50 -0500

    ruta category

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ Route::get('/', function () {
 Route::get('/categories', [CategoryController ::class,'index'])->name('categories.index');
 Route::get('/categories', [CategoryController ::class,'store'])->name('categories.store');
 Route::get('/categories/create', [CategoryController ::class,'create'])->name('categories.create');
-Route::get('/categories/{category}', [CategoryController ::class,'create'])->name('categories.create');
+Route::get('/categories/{category}', [CategoryController ::class,'destroy'])->name('categories.destroy');
+Route::get('/categories/{category}', [CategoryController ::class,'update'])->name('categories.update');
 Route::get('/categories/{category}/edit', [CategoryController ::class,'edit'])->name('categories.edit');
